03828d3844f2d1161b11f821b0320e8b4b778c36
Emilian Manescu <manescuemilian@gmail.com>
2024-04-05 19:55:54 +0300

    Added logging using Logback

--- src/main/java/com/api/storemanagement/operations/ProductService.java
+++ src/main/java/com/api/storemanagement/operations/ProductService.java
@@ -3,6 +3,8 @@ package com.api.storemanagement.operations;
 import com.api.storemanagement.exceptions.ProductNotFoundException;
 import com.api.storemanagement.product.Product;
 import com.api.storemanagement.product.ProductRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,21 +15,31 @@ public class ProductService {
 
 	private final ProductRepository productRepository;
 
+	private static final Logger logger = LoggerFactory.getLogger(ProductService.class);
+
 	@Autowired
 	public ProductService(ProductRepository productRepository) {
 		this.productRepository = productRepository;
+		logger.info("ProductService initialized");
 	}
 
 	public Product addProduct(Product product) {
+		logger.info("Adding a new product: {}", product.getName());
+
+		if (productRepository.existsByName(product.getName())) {
+			logger.warn("Attempting to add a product that already exists with name: {}", product.getName());
+		}
 		return productRepository.save(product);
 	}
 
 	public Product findProduct(Long productId) {
+		logger.info("Retrieving product with ID: {}", productId);
 		return productRepository.findById(productId)
 				.orElseThrow(() -> new ProductNotFoundException("Product not found with id: " + productId));
 	}
 
 	public Product updateProduct(Long productId, Product updatedProduct) {
+		logger.info("Updating product with ID: {}", productId);
 		Product existingProduct = productRepository.findById(productId)
 				.orElseThrow(() -> new ProductNotFoundException("Product not found with id: " + productId));
 
@@ -40,13 +52,16 @@ public class ProductService {
 	}
 
 	public void removeProduct(Long productId) {
+		logger.info("Removing product with ID: {}", productId);
 		if (!productRepository.existsById(productId)) {
+			logger.warn("Trying to delete a non-existent product with ID: {}\", productId)");
 			throw new ProductNotFoundException("Product not found with id: " + productId);
 		}
 		productRepository.deleteById(productId);
 	}
 
 	public List<Product> listAllProducts() {
+		logger.debug("Listing all products");
 		return productRepository.findAll();
 	}
 }
--- src/main/java/com/api/storemanagement/product/ProductRepository.java
+++ src/main/java/com/api/storemanagement/product/ProductRepository.java
@@ -3,4 +3,5 @@ package com.api.storemanagement.product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+	Boolean existsByName(String name); //Checks if there are any records by name
 }
